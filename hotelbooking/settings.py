e21d99e0428de505869a89d525ff2d9ee94c8cb7
Ukwuani Bencollins <ukwuanisomto189@gmail.com>
2026-05-19 23:46:11 +0100

    added postgresql support

--- hotelbooking/settings.py
+++ hotelbooking/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -62,11 +63,12 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'hotelbooking.wsgi.application'
 
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=f'sqlite:///{BASE_DIR / "db.sqlite3"}',
+        conn_max_age=600
+    )
 }
 
 AUTH_PASSWORD_VALIDATORS = [
